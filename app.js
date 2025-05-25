1321940689419c35d8265c4d0be193db00bfdad0
Tradersharing <ardiyulistyanto3@gmail.com>
2025-05-26 04:18:32 +0700

    Delete app.js

--- app.js
+++
@@ -1,5 +0,0 @@
-// app.js
-document.addEventListener('DOMContentLoaded', () => {
-  const dashboard = document.getElementById('dashboard');
-  dashboard.innerHTML += '<p>Selamat datang di dashboard penambangan!</p>';
-});
